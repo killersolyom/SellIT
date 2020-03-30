ace48b3e91d2879c16dbe0ed174726b5fa5673c9
Zsolti <szazsolti@yahoo.com>
2020-03-30 18:57:12 +0300

    Updated DefaultViewHolder.

--- app/src/main/java/com/sell/it/Fragment/AdvertisementFragment.java
+++ app/src/main/java/com/sell/it/Fragment/AdvertisementFragment.java
@@ -8,13 +8,16 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sell.it.Adapter.ItemAdapter;
+import com.sell.it.Model.BaseAdvertisementItem;
+import com.sell.it.Model.BaseItem;
+import com.sell.it.Model.FakeItem;
 import com.sell.it.R;
 
 public class AdvertisementFragment extends BaseFragment {
 
-    private RecyclerView recycle;
-    private GridLayoutManager layoutManager;
-    private ItemAdapter advertisementAdapter;
+    private RecyclerView mRecycle;
+    private GridLayoutManager mLayoutManager;
+    private ItemAdapter mAdvertisementAdapter;
 
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container) {
@@ -23,15 +26,22 @@ public class AdvertisementFragment extends BaseFragment {
 
     @Override
     protected void findView(View view) {
-        recycle = view.findViewById(R.id.advertisement_recycler_view);
+        mRecycle = view.findViewById(R.id.advertisement_recycler_view);
     }
 
     @Override
     protected void initComponents() {
-        layoutManager = new GridLayoutManager(this.getContext(), 2);
-        recycle.setLayoutManager(layoutManager);
-        advertisementAdapter = new ItemAdapter();
-        recycle.setAdapter(advertisementAdapter);
+        mLayoutManager = new GridLayoutManager(this.getContext(), 2);
+        mRecycle.setLayoutManager(mLayoutManager);
+        mAdvertisementAdapter = new ItemAdapter();
+        mRecycle.setAdapter(mAdvertisementAdapter);
+        BaseAdvertisementItem advItem = new BaseAdvertisementItem();
+        advItem.setTitle("Ez egy adv");
+        FakeItem fakeItem = new FakeItem();
+        fakeItem.setTitle("Ez nem egy adv, ez hiba.");
+        mAdvertisementAdapter.addItem(advItem);
+        mAdvertisementAdapter.addItem(fakeItem);
+
     }
 
 }
