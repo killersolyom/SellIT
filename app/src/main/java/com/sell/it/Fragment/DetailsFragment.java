6da12df629500c3c7f169d447b2d42a6ca14cdca
szazsolti <szazsolti10@gmail.com>
2020-04-16 19:22:52 +0300

    Created Details fragment.

---
+++ app/src/main/java/com/sell/it/Fragment/DetailsFragment.java
@@ -0,0 +1,17 @@
+package com.sell.it.Fragment;
+
+import android.view.View;
+
+import com.sell.it.R;
+
+public class DetailsFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_details;
+    }
+
+    @Override
+    protected void findView(View view) {
+
+    }
+}
