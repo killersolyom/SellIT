76912492e7d1e853add91f680824affd6409067c
szazsolti <szazsolti10@gmail.com>
2020-04-16 19:19:37 +0300

    Created Profile fragment.

---
+++ app/src/main/java/com/sell/it/Fragment/ProfileFragment.java
@@ -0,0 +1,17 @@
+package com.sell.it.Fragment;
+
+import android.view.View;
+
+import com.sell.it.R;
+
+public class ProfileFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_profile;
+    }
+
+    @Override
+    protected void findView(View view) {
+
+    }
+}
