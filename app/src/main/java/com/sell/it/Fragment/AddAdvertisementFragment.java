8335205ac99312eb29417ec1da6c504b3563e09f
szazsolti <szazsolti10@gmail.com>
2020-04-16 19:15:39 +0300

    Created AddAdvertisement fragment.

---
+++ app/src/main/java/com/sell/it/Fragment/AddAdvertisementFragment.java
@@ -0,0 +1,17 @@
+package com.sell.it.Fragment;
+
+import android.view.View;
+
+import com.sell.it.R;
+
+public class AddAdvertisementFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_add_advertisement;
+    }
+
+    @Override
+    protected void findView(View view) {
+
+    }
+}
