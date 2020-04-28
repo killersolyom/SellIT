dae663645ccc1ce5a46406d0a6a0f5c2c8d71c04
szazsolti <szazsolti10@gmail.com>
2020-04-28 18:44:58 +0300

    Updated login.

--- app/src/main/java/com/sell/it/Utility/DatabaseManager.java
+++ app/src/main/java/com/sell/it/Utility/DatabaseManager.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sell.it.Model.Event;
 import com.sell.it.Model.User;
 
@@ -28,7 +34,7 @@ public class DatabaseManager {
         mDatabase = mFirebaseDatabase.getReference();
     }
 
-    public static void createUser(User user){
+    public static void createUser(User user) {
         mAuth.createUserWithEmailAndPassword(user.getEmailAddress(), user.getPassword())
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
@@ -38,26 +44,45 @@ public class DatabaseManager {
                         Bundle extraBundle = BundleUtil.createBundle(USER_KEY, user);
                         EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE, Event.ACTION_REGISTRATION_SUCCESS, extraBundle), true);
                     } else {
-                        EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE,Event.ACTION_REGISTRATION_FAIL), true);
+                        EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE, Event.ACTION_REGISTRATION_FAIL), true);
                         Log.w(TAG, "createUserWithEmail:failure", task.getException());
                     }
                 });
     }
 
-    public static void loginUser(String emailAddress, String password){
-        mAuth.signInWithEmailAndPassword(emailAddress,password)
+    public static void loginUser(String emailAddress, String password) {
+        mAuth.signInWithEmailAndPassword(emailAddress, password)
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()){
-                        if (DataManager.isUserExist(emailAddress, password)) {
-                            User loggedInUser =  DataManager.getUser();
-                            Bundle extraBundle = new Bundle();
-                            extraBundle.putSerializable(USER_KEY, loggedInUser);
-                            EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE,Event.ACTION_LOGIN_SUCCESS,extraBundle),true);
-                        }
+                    if (task.isSuccessful()) {
+                        FirebaseUser currentUser = mAuth.getCurrentUser();
+                        getUserFromDatabase(currentUser.getUid());
                     } else {
-                        EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE,Event.ACTION_LOGIN_FAIL),true);
+                        EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE, Event.ACTION_LOGIN_FAIL), true);
                         Log.w(TAG, "loginUserWithEmail:failure", task.getException());
                     }
                 });
+    }
+
+    private static void getUserFromDatabase(String uId){
+        mDatabase.child(FIREBASE_USER_KEY).child(uId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String email = dataSnapshot.child("emailAddress").getValue().toString();
+                String firstName = dataSnapshot.child("firstName").getValue().toString();
+                String lastName = dataSnapshot.child("lastName").getValue().toString();
+                String username = dataSnapshot.child("username").getValue().toString();
+                String password = dataSnapshot.child("password").getValue().toString();
+
+                User loggedInUser = new User(email, firstName, lastName, username, password);
+                Bundle extraBundle = new Bundle();
+                extraBundle.putSerializable(USER_KEY, loggedInUser);
+                EventDispatcher.offerEvent(new Event(Event.TYPE_FIREBASE, Event.ACTION_LOGIN_SUCCESS, extraBundle), true);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
