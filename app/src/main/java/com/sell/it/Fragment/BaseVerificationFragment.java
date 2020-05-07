630d682b3519da201eeef579af2b295c1fcf668d
szazsolti <szazsolti10@gmail.com>
2020-05-07 17:22:57 +0300

    Added verification functionality.

--- app/src/main/java/com/sell/it/Fragment/BaseVerificationFragment.java
+++ app/src/main/java/com/sell/it/Fragment/BaseVerificationFragment.java
@@ -3,6 +3,9 @@ package com.sell.it.Fragment;
 import android.view.View;
 
 import com.sell.it.Model.Event;
+import com.sell.it.R;
+import com.sell.it.Utility.FragmentNavigation;
+import com.sell.it.Utility.SnackBarUtility;
 
 public class BaseVerificationFragment extends BaseFragment {
     @Override
@@ -19,6 +22,14 @@ public class BaseVerificationFragment extends BaseFragment {
     public boolean onEvent(Event event) {
         switch (event.getEventType()) {
             case Event.TYPE_FIREBASE:
+                switch (event.getAction()) {
+                    case Event.ACTION_VERIFICATION_FAIL:
+                        SnackBarUtility.showWithText(R.string.invalid_id, true);
+                        return true;
+                    case Event.ACTION_VERIFICATION_SUCCESS:
+                        FragmentNavigation.showAddAdvertisementFragment();
+                        return true;
+                }
                 return true;
         }
         return false;
