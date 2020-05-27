b99a03a27fecffcfcd6f8f8faba9bdd72dce2a78
Solyom <killersolyom@gmail.com>
2020-05-27 17:26:14 +0300

    bugfixing

--- app/src/main/java/com/sell/it/Fragment/AddAdvertisementFragment.java
+++ app/src/main/java/com/sell/it/Fragment/AddAdvertisementFragment.java
@@ -357,6 +357,7 @@ public class AddAdvertisementFragment extends BaseFragment {
             mItemAdapter.clearItems();
             addDataInputFields(mItemType);
         } else {
+            mItemAdapter.clearItems();
             addCategorySelectorField();
         }
     }
